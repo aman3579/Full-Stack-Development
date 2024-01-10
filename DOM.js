3ea4be28244837ccc9b47f3e0af8c3e2b71a08cf
Aman <amangupta3579@gmail.com>
2024-01-10 16:04:54 +0530

    DOM-Methods

---
+++ DOM.js
@@ -0,0 +1,24 @@
+// //DOM creates objects and  dictates how the structure od document will be created.
+// //browser generates render tree
+// //rasterisation
+// // window.alert("Hello !! alert")
+// // console.error("errr foundo")
+// // console.log(document)
+// console.log(document.body)
+
+// const heading = document.getElementById('heading');
+// heading.innerHTML = `this is text`
+// this is bold
+// this is italic`
+// console.log(heading)
+// console.dir(heading) //to get information in the object form
+
+// const item2 = document.getElementsByClassName('item1')
+// item2.innerText = 'jug';
+// const tag = document.getElementsByTagName('li')
+// console.log(tag)
+
+
+//queryselector uses like CSS selectors
+const element = document.querySelector('h1')
+console.log(element)
---
+++ DOM_Methods.js
@@ -0,0 +1,9 @@
+const heading = document.querySelector('h1')
+// console.log(heading.innerHTML)
+//innerHTML gives the content inside a atg
+// const body = document.querySelector('body')
+// console.log(body.innerHTML)
+// heading.innerHTML = "Add stuff to heading using innerHTML"
+
+heading.innerHTML = `
+<span style="color:red">Hello</span> World!!`
