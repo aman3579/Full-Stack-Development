7b7838e66c8699b30aa711032d81f083fe1ff419
Aman <amangupta3579@gmail.com>
2024-01-12 17:54:30 +0530

    Rock paper scissors

--- eventListeners.js
+++ eventListeners.js
@@ -1,27 +1,28 @@
-const body = document.querySelector('body')
-const textbox = document.querySelector('h1 span')
-const heading = document.querySelector('h1')
-let count=0;
+// const body = document.querySelector('body')
+// const textbox = document.querySelector('h1 span')
+// const heading = document.querySelector('h1')
+// let count=0;
+// const input = document.querySelector('input')
+// // heading.addEventListener('click', ()=>{
+// //     console.log("You clicked!!")
+// //     textbox.innerText = ++count;
+// // })
+
+// //for noting the key pressed from keyboard
+// // body.addEventListener('keydown',(event)=>{
+// //     console.log(event.key)
+// // })
+
+// //for cursor position
+// // body.addEventListener('mousemove',(event)=>{
+// //     console.log({
+// //         x:event.clientX,
+// //         y:event.clientY
+// //     })
+// // })
+
+// highlights th border of input
 const input = document.querySelector('input')
-// heading.addEventListener('click', ()=>{
-//     console.log("You clicked!!")
-//     textbox.innerText = ++count;
-// })
-
-//for noting the key pressed from keyboard
-// body.addEventListener('keydown',(event)=>{
-//     console.log(event.key)
-// })
-
-//for cursor position
-// body.addEventListener('mousemove',(event)=>{
-//     console.log({
-//         x:event.clientX,
-//         y:event.clientY
-//     })
-// })
-
-//highlights th border of input
 // input.addEventListener('focusin',()=>{
 //     console.log("input is focused in")
 // })
@@ -31,7 +32,18 @@ const input = document.querySelector('input')
 // window.addEventListener('blur',()=>{
 //     console.log("tab changed")
 // })
-
-window.addEventListener('scroll',()=>{
-    console.log("scroll detected")
+window.addEventListener('copy',()=>{
+    console.log("Content copied detected!")
 })
+
+// window.addEventListener('scroll',()=>{
+//     console.log("scroll detected")
+// })
+
+// let dog = {name:'Bobo',age:3};
+// function speak(animal){
+//     animal.age = 5;
+//     return "woof";
+// }
+// speak(dog)
+// console.log(dog)
